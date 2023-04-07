d6c3197b01b58f4e55eb9a42063317c14187cdf2
Anis Baloch <balochanis82@gmail.com>
2023-04-07 18:40:39 +0400

    Assignment 1 complete

--- Assignment-01/js/script.js
+++ Assignment-01/js/script.js
@@ -1,11 +1,143 @@
 //1
-let userAge = prompt("How old are you?");
-if(userAge <= 10)
-{
-    console.log("Welcome");
-}
+// let num1 = +prompt("Enter first number");
+// let num2 = +prompt("Enter second number");
 
-else
-{
-    console.log("You are not allowed");
-}
+// if (num1 > num2) {
+//     console.log(num1);
+// }
+
+// else {
+//     console.log(num2);
+// }
+
+
+
+
+
+//2
+// let userInput = +prompt("Type any number in positive or negative");
+// if (userInput >= 1) {
+//     console.log("The sign is +");
+// }
+
+// else {
+//     console.log("The sign is -");
+// }
+
+
+
+//3
+// let usrInput1 = +prompt("Enter First number");
+// let usrInput2 = +prompt("Enter Second number");
+// let usrInput3 = +prompt("Enter Third number");
+// let usrInput4 = +prompt("Enter Forth number");
+// let usrInput5 = +prompt("Enter Fifth number");
+
+// let biggestNum = usrInput1;
+
+// if (usrInput2 > biggestNum) {
+//     biggestNum = usrInput2;
+// }
+
+// else if (usrInput3 > biggestNum) {
+//     biggestNum = usrInput3;
+// }
+
+// else if (usrInput4 > biggestNum) {
+//     biggestNum = usrInput4;
+// }
+
+// else if (usrInput5 > biggestNum) {
+//     biggestNum = usrInput5;
+// }
+
+
+// console.log(biggestNum);
+
+
+
+
+//4
+
+// for(let i=0; i<=15; i++)
+// {
+
+//     if(i%2 == 0)
+//     {
+//         console.log(i + " is even");
+
+//     }
+
+//     else{
+//         console.log(i + " is odd");
+//     }
+
+
+// }
+
+
+
+//5
+// let studentMarks = Math.floor(Math.random() * 100);
+// let studentGrade;
+// console.log(studentMarks);
+
+// if(studentMarks >= 90)
+// {
+//     studentGrade = "A";
+// }
+
+// else if(studentMarks >= 80)
+// {
+//     studentGrade = "B";
+// }
+
+// else if(studentMarks >= 70)
+// {
+//     studentGrade = "C";
+// }
+// else if(studentMarks >= 60)
+// {
+//     studentGrade = "D";
+// }
+
+// else if(studentMarks >= 50 || studentMarks <50)
+// {
+//     studentGrade = "F";
+// }
+
+// console.log(studentGrade);
+
+
+//6
+// for (let i = 1; i <= 100; i++) {
+//     if (i % 3 === 0 && i % 5 === 0) {
+//       console.log("FizzBuzz");
+//     }
+//     else if (i % 3 === 0) {
+//       console.log("Fizz");
+//     }
+//     else if (i % 5 === 0) {
+//       console.log("Buzz");
+//     }
+//     else {
+//       console.log(i);
+//     }
+//   }
+
+
+//7
+// let numRows = 5;
+
+// let pattern = "";
+
+// for (let row = 1; row <= numRows; row++) {
+
+//     for (let star = 0; star < row; star++) {
+//         pattern += "*";
+//     }
+
+//     pattern += "\n";
+// }
+
+// console.log(pattern);
